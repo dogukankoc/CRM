db97773a3aea21a881700cc7483a3667e4934450
gglnyrdm <gulin.yardimoglu@etiya.com>
2024-05-09 21:56:25 +0300

    add ContactMediumController

---
+++ customerservice/src/main/java/com/etiyacrm/customerservice/controllers/ContactMediumController.java
@@ -0,0 +1,50 @@
+package com.etiyacrm.customerservice.controllers;
+
+import com.etiyacrm.customerservice.services.abstracts.ContactMediumService;
+import com.etiyacrm.customerservice.services.dtos.requests.contactMedium.CreateContactMediumRequest;
+import com.etiyacrm.customerservice.services.dtos.requests.contactMedium.UpdateContactMediumRequest;
+import com.etiyacrm.customerservice.services.dtos.responses.contactMedium.CreatedContactMediumResponse;
+import com.etiyacrm.customerservice.services.dtos.responses.contactMedium.DeletedContactMediumResponse;
+import com.etiyacrm.customerservice.services.dtos.responses.contactMedium.GetContactMediumResponse;
+import com.etiyacrm.customerservice.services.dtos.responses.contactMedium.UpdatedContactMediumResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@AllArgsConstructor
+@CrossOrigin()
+@RequestMapping("customerservice/api/v1/contact-mediums")
+public class ContactMediumController {
+    private ContactMediumService contactMediumService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Operation(summary = "Add")
+    public CreatedContactMediumResponse add(@Valid @RequestBody CreateContactMediumRequest createContactMediumRequest){
+        return contactMediumService.add(createContactMediumRequest);
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "getById")
+    public GetContactMediumResponse getById(@PathVariable long id){
+        return contactMediumService.getById(id);
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Update")
+    public UpdatedContactMediumResponse update(@Valid @RequestBody UpdateContactMediumRequest updateContactMediumRequest,
+                                               @PathVariable long id){
+        return contactMediumService.update(id, updateContactMediumRequest);
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Operation(summary = "Delete")
+    public DeletedContactMediumResponse delete(@PathVariable long id){
+        return contactMediumService.delete(id);
+    }
+}
