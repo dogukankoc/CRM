5f2df456a36f0f44f52c0d73e341ced94d4242f6
Dogukan <dk.dogukankoc@gmail.com>
2024-05-13 11:47:41 +0300

    Update get individual customer response

--- customerservice/src/main/java/com/etiyacrm/customerservice/services/dtos/responses/individualCustomer/GetIndividualCustomerResponse.java
+++ customerservice/src/main/java/com/etiyacrm/customerservice/services/dtos/responses/individualCustomer/GetIndividualCustomerResponse.java
@@ -4,7 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 
 @Data
 @AllArgsConstructor
@@ -18,6 +18,6 @@ public class GetIndividualCustomerResponse {
     private String motherName;
     private String fatherName;
     private String nationalityIdentity;
-    private LocalDateTime birthDate;
-    private LocalDateTime createdDate;
+    private LocalDate birthDate;
+//    private LocalDateTime createdDate;
 }
